e38a79d7c2ceb75abec6d5b78054342bf5af2a96
Burkhard Weber <Burkhard.Weber@perceptivesoftware.com>
2014-12-02 22:10:44 +0100

    OO render implementation

--- dynamic-render-service/dynamic-render-service-bundle/dynamic-render-service-bundle-service/dynamic-render-service-bundle-service-oo/src/main/java/com/psd/rendering/drs/osgi/service/Activator.java
+++ dynamic-render-service/dynamic-render-service-bundle/dynamic-render-service-bundle-service/dynamic-render-service-bundle-service-oo/src/main/java/com/psd/rendering/drs/osgi/service/Activator.java
@@ -1,5 +1,9 @@
 package com.psd.rendering.drs.osgi.service;
 
+import org.artofsolving.jodconverter.OfficeDocumentConverter;
+import org.artofsolving.jodconverter.document.DocumentFormatRegistry;
+import org.artofsolving.jodconverter.office.DefaultOfficeManagerConfiguration;
+import org.artofsolving.jodconverter.office.OfficeManager;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
@@ -8,15 +12,22 @@ import com.psd.rendering.drs.osgi.api.RenderService;
 
 public class Activator implements BundleActivator {
     private ServiceRegistration reg;
+    private OfficeManager officeManager;
 
     @Override
     public void start(BundleContext context) throws Exception {
-        reg = context.registerService(RenderService.class.getName(), new RenderServiceOOImpl(), null);
+    	
+    	officeManager = new DefaultOfficeManagerConfiguration().setOfficeHome("C:\\Program Files (x86)\\OpenOffice.org 3").buildOfficeManager();
+        officeManager.start();
+        
+        reg = context.registerService(RenderService.class.getName(), new RenderServiceOOImpl(officeManager), null);
         System.out.println("Registration: " + reg.toString());
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
+    	
+    	officeManager.stop();
         reg.unregister();
     }
 }
